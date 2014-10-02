e7ac9e7fe7908b1d49690a6fdf55b8bbfeb3b21c
ivy labossiere <ivy.labossiere@hotmail.ca>
2014-10-02 10:49:21 -0400

    added code to make buttons work
    added code in html css and js

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
 // JavaScript Document
+var salesdetailsleft = $('.sales__detailsleft');
+var salesleft = $('.salesleft');
+
+var buttonClickHandlerleft = function() {
+	var currentState = salesdetailsleft.attr('data-state');
+	if (currentState == 'active') {
+		salesdetailsleft.attr('data-state', 'inactive');
+	} else {
+		salesdetailsleft.attr('data-state', 'active');	
+	}
+}
+salesleft.on('click', buttonClickHandlerleft);
+
+var salesdetailsright = $('.sales__detailsright');
+var salesright = $('.salesright');
+
+var buttonClickHandlerright = function() {
+	var currentState = salesdetailsright.attr('data-state');
+	if (currentState == 'active') {
+		salesdetailsright.attr('data-state', 'inactive');
+	} else {
+		salesdetailsright.attr('data-state', 'active');	
+	}
+}
+salesright.on('click', buttonClickHandlerright);
